6de8c9e13adafb7df8fe129257720be2e2012954
MyNameIsTrez <sbos@student.codam.nl>
2023-08-10 18:43:31 +0200

    Add signal.c

---
+++ experiments/signal.c
@@ -0,0 +1,39 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void sig_handler(int sig)
+{
+	(void)sig;
+	printf("Inside sig_handler\n");
+}
+
+// Do Ctrl+C to print "Inside sig_handler"
+// Do Ctrl+\ to quit the program
+// gcc signal.c -Wall -Wextra -Werror -Wpedantic -Wfatal-errors -g && ./a.out
+int main(void)
+{
+	int sig = SIGINT;
+	// Can't be caught or ignored
+	// int sig = SIGKILL;
+
+	// These calls both ignore the SIGINT
+	signal(sig, sig_handler);
+	// signal(sig, SIG_IGN);
+
+	// Resets to the default behavior
+	// signal(sig, SIG_DFL);
+
+	// signal(sig, SIG_IGN);
+
+	while (1)
+	{
+		printf("Loop\n");
+		sleep(1);
+	}
+
+    return EXIT_SUCCESS;
+}
