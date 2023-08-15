6f1491ade663b6df4963764160d9a03d0ed88882
MyNameIsTrez <sbos@student.codam.nl>
2023-08-15 17:06:54 +0200

    Use SO_REUSEADDR

--- experiments/server.c
+++ experiments/server.c
@@ -1,3 +1,4 @@
+#include <arpa/inet.h>
 #include <errno.h>
 #include <netinet/in.h>
 #include <stdarg.h>
@@ -8,12 +9,14 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define MAX_RECEIVED_LEN 4096
-#define MAX_SENT_LEN 4096
-
 #define SERVER_PORT 18000
 
 #define MAX_CONNECTION_QUEUE_LENGTH 10
+
+#define MAX_RECEIVED_LEN 4096
+#define MAX_SENT_LEN 4096
+
+#define TCP 0
 
 void die(const char *fmt, ...)
 {
@@ -38,7 +41,7 @@ void die(const char *fmt, ...)
 	exit(EXIT_FAILURE);
 }
 
-char *bin2hex(uint8_t *input, size_t len)
+char *bin2hex(char *input, size_t len)
 {
 	// A hexit is a hexadecimal digit
 	char *hexits = "0123456789ABCDEF";
@@ -56,6 +59,7 @@ char *bin2hex(uint8_t *input, size_t len)
 
 	for (size_t i = 0; i < len; i++)
 	{
+		// This shifting and masking assumes that input is unsigned
 		result[i * 3 + 0] = hexits[input[i] >> 4];
 		result[i * 3 + 1] = hexits[input[i] & 0x0F];
 		result[i * 3 + 2] = ' ';
@@ -69,10 +73,16 @@ char *bin2hex(uint8_t *input, size_t len)
 int main(void)
 {
 	int socket_fd;
-	if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	if ((socket_fd = socket(AF_INET, SOCK_STREAM, TCP)) < 0)
 	{
 		die("socket");
 	}
+
+	// "the parameter should be non-zero to enable a boolean option"
+	int option = 1;
+	// Enables local address reuse
+	// This prevents socket() calls from possibly returning an error on server restart
+	setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
 
 	struct sockaddr_in servaddr;
 	bzero(&servaddr, sizeof(servaddr));
@@ -90,8 +100,8 @@ int main(void)
 		die("listen");
 	}
 
-	uint8_t received[MAX_RECEIVED_LEN + 1];
-	uint8_t sent[MAX_SENT_LEN + 1];
+	char received[MAX_RECEIVED_LEN + 1];
+	char *sent = "HTTP/1.0 200 OK\r\n\r\n<H1>Hello";
 
 	while (true)
 	{
@@ -107,13 +117,37 @@ int main(void)
 		// it should initially contain the amount of space pointed to by address;
 		// on return it will contain the actual length (in bytes) of the address returned.
 		// This call is used with connection-based socket types, currently with SOCK_STREAM.
-		// struct sockaddr address;
-		// socklen_t address_len;
-		// client_fd = accept(socket_fd, &address, &address_len);
+		struct sockaddr address;
+		socklen_t address_len = sizeof(address);
+		int client_fd = accept(socket_fd, &address, &address_len);
+		printf("Address length: %d\n", address_len);
+
+		// // IPv4 will also fit with INET6_ADDRSTRLEN, since it is 46:
+		// // https://stackoverflow.com/a/7477384/13279557
+		// char address_string[INET6_ADDRSTRLEN];
+		// // TODO: Think of better name for cast_address
+		// void *cast_address;
+
+		// // Not sure when/if I should be casting ot
+		// if (address.sa_family == AF_INET)
+		// {
+		// 	// IPv4
+		// 	cast_address = &((struct sockaddr_in *)address.sa_data)->sin_addr;
+		// }
+		// else
+		// {
+		// 	// IPv6
+		// 	cast_address = &((struct sockaddr_in6 *)address.sa_data)->sin6_addr;
+		// }
+
+		// // Network representation to printable string
+		// inet_ntop(address.sa_family, cast_address, address_string, INET6_ADDRSTRLEN);
+
+		// printf("Address: %s\n", address_string);
 
 		// accept() blocks until an incoming connection arrives
 		// It returns a file descriptor to the connection
-		int client_fd = accept(socket_fd, NULL, NULL);
+		// int client_fd = accept(socket_fd, NULL, NULL);
 
 		// Zero out the receive buffer to make sure it ends up null-terminated
 		bzero(received, MAX_RECEIVED_LEN + 1);
@@ -122,7 +156,7 @@ int main(void)
 		ssize_t bytes_read;
 		while ((bytes_read = read(client_fd, received, MAX_RECEIVED_LEN)) > 0)
 		{
-			printf("\nbin2hex: %s\n\nreceived: %s", bin2hex(received, bytes_read), received);
+			printf("\nbin2hex:\n%s\n\nReceived:\n%s", bin2hex(received, bytes_read), received);
 
 			// TODO: This is a hacky way to detect the end of the message
 			if (received[bytes_read - 1] == '\n')
@@ -137,11 +171,9 @@ int main(void)
 			die("read");
 		}
 
-		// Send a response
-		snprintf((char *)sent, sizeof(sent), "HTTP/1.0 200 OK\r\n\r\nHello");
-
+		// Send the response
 		// TODO: Don't ignore errors
-		write(client_fd, sent, strlen((char *)sent));
+		write(client_fd, sent, strlen(sent));
 		close(client_fd);
 	}
 
