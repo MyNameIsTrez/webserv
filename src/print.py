fae9406e7b412710df8f0e66f00d083acaef3061
MyNameIsTrez <welfje@gmail.com>
2023-09-08 20:46:51 +0200

    Let print.py close stdout and sleep for 5 seconds

--- src/print.py
+++ src/print.py
@@ -1,4 +1,4 @@
-import json, os, sys
+import json, os, sys, time
 
 
 def main():
@@ -6,9 +6,18 @@ def main():
 
     print(f"argv: {sys.argv}")
 
+    # TODO: Test closing stdin
+
     print(f"stdin: {sys.stdin.readlines()}")
 
     print(f"env: {json.dumps(dict(os.environ), sort_keys=True, indent=4)}")
+
+    print("", end="", flush=True)
+
+    os.close(sys.stdout.fileno())
+    sys.stdout.close()
+
+    time.sleep(5)
 
     # TODO: Test that this sends a "CGI failed" page to the client
     exit(2)
