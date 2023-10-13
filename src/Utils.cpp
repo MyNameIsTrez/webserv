8acd8b9b4249112205c20fac7d990f5600227e4b
MyNameIsTrez <welfje@gmail.com>
2023-10-13 12:29:05 +0200

    Fix startsWith()

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -2,5 +2,5 @@
 
 bool Utils::startsWith(const std::string &haystack, const std::string &needle)
 {
-	return haystack.rfind(needle, 0) == std::string::npos;
+	return haystack.rfind(needle, 0) != std::string::npos;
 }
