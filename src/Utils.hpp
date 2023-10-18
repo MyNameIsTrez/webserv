05e17f0b7ab5a087a9a46775bd2942c20055a8dc
MyNameIsTrez <welfje@gmail.com>
2023-10-18 15:07:00 +0200

    Add max and wraparound check in parseNumber()

--- src/Utils.hpp
+++ src/Utils.hpp
@@ -6,7 +6,7 @@ namespace Utils
 {
 	bool startsWith(const std::string &haystack, const std::string &needle);
 	template <class T>
-	bool parseNumber(const std::string &str, T &number);
+	bool parseNumber(const std::string &str, T &number, T max);
 }
 
 #include "Utils.tpp"
