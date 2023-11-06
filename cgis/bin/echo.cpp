2122e0ffd03aeca1c04ba4f65053f78b7a67245c
MyNameIsTrez <welfje@gmail.com>
2023-11-06 11:30:00 +0100

    Fix /cgis/bin/echo for now

--- cgis/bin/echo.cpp
+++ cgis/bin/echo.cpp
@@ -2,21 +2,23 @@
 #include <unistd.h>
 #include <iostream>
 
-// c++ echo.cpp -Wall -Wextra -Werror -Wpedantic -Wshadow -Wfatal-errors -g -fsanitize=address,undefined -o echo && echo foo | ./echo bar
+// c++ echo.cpp -Wall -Wextra -Werror -Wpedantic -Wshadow -Wfatal-errors -g -fsanitize=address,undefined -o echo && echo foo | ./echo
 int main(int argc, char *argv[], char *envp[])
 {
-	(void)argc;
-	while (*argv) {
-		std::cout << *argv << std::endl;
-		argv++;
+	for (size_t i = 0; i < static_cast<size_t>(argc); i++) {
+		std::cout << "argv[" << i << "]: " << argv[i] << std::endl;
 	}
+
 	std::cout << std::endl;
-	while (*envp) {
-		std::cout << *envp << std::endl;
-		envp++;
+
+	for (size_t i = 0; envp[i]; i++) {
+		std::cout << "envp[" << i << "]: " << envp[i] << std::endl;
 	}
+
 	std::cout << std::endl;
-	for (std::string line; std::getline(std::cin, line);) {
-        std::cout << line << std::endl;
+
+	size_t i = 0;
+	for (std::string line; std::getline(std::cin, line); i++) {
+        std::cout << "line[" << i << "]: " << line << std::endl;
     }
 }
--- cgis/bin/execve.cpp
+++ cgis/bin/execve.cpp
@@ -1,11 +1,11 @@
 #include <cstdlib>
 #include <unistd.h>
 
-// c++ execve.cpp -Wall -Wextra -Werror -Wpedantic -Wshadow -Wfatal-errors -g -fsanitize=address,undefined -o execve && echo foo | ./execve echo /foo.txt
+// c++ execve.cpp -Wall -Wextra -Werror -Wpedantic -Wshadow -Wfatal-errors -g -fsanitize=address,undefined -o execve && echo foo | ./execve echo
 int main(int argc, char *argv[], char *envp[])
 {
-	if (argc != 3)
+	if (argc != 2)
 		return EXIT_FAILURE;
-	char * const args[] = {argv[1], argv[2], NULL};
+	char * const args[] = {argv[1], NULL};
 	execve(argv[1], args, envp);
 }
